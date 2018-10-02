6b89e6a49f646f21671c9ab971858ec6dd3823b4
ksa <karapetyan.sa@gmail.com>
2018-10-02 15:21:05 +0300

    Fix Header for CharacterPage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {Route} from "react-router";
 import {styled} from "../core/styledComponents";
 // import {Button} from "../ui/Button";
 import {Filters} from "./Filters";
@@ -16,14 +17,15 @@ const HeaderContainer = styled<{}, "header">("header")`
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
     transition: 0.3s;
     padding: 5px 0px 5px 0px;
+    min-height: 48px;
 `;
 
 export class Header extends React.Component {
     public render() {
         return (
             <HeaderContainer>
-                <Filters />
-                <ReloadButton />
+                <Route exact={true} path="/" component={Filters} />
+                <Route exact={true} path="/" component={ReloadButton} />
             </HeaderContainer>
         );
     }
