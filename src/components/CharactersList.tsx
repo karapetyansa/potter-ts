ced11c445ced35071ffac479246946a0cfd5c0ad
ksa <karapetyan.sa@gmail.com>
2018-10-02 10:48:51 +0300

    Add react-virtualized

--- src/components/CharactersList.tsx
+++ src/components/CharactersList.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import {connect} from "react-redux";
+import {List, ListRowRenderer, WindowScroller} from "react-virtualized";
 import {Dispatch} from "redux";
 import {createSelector} from "reselect";
-import {ICharacter} from "../api/dto/characters";
 import {IAppState} from "../core/store/mainReducer";
 import {loadCharacters} from "../modules/characters/charactersActions";
 import {Filters} from "./Filters";
@@ -35,59 +35,49 @@ const mapDispatch = (dispatch: Dispatch) => ({
 
 interface IProps extends ReturnType<typeof mapState>, ReturnType<typeof mapDispatch> {}
 
-interface IState {
-    currentPage: number;
-    initLoad: boolean;
-    currentScrollPage: number;
-}
-
-const scrollPortion = 16;
-
 export const CharactersList = connect(
     mapState,
     mapDispatch
 )(
-    class extends React.Component<IProps, IState> {
-        private containerRef: React.RefObject<HTMLUListElement>;
-        constructor(props: IProps) {
-            super(props);
-            this.state = {currentPage: 0, initLoad: true, currentScrollPage: 0};
-            this.containerRef = React.createRef();
-        }
+    class extends React.Component<IProps> {
         public componentDidMount() {
-            window.addEventListener("scroll", this.onScroll, false);
-
             this.props.reload();
         }
-        public componentWillUnmount() {
-            window.removeEventListener("scroll", this.onScroll, false);
-        }
+
         public render() {
             const {reload, charactersList} = this.props;
-            const {currentScrollPage} = this.state;
-            const items = charactersList.slice(0, (currentScrollPage + 1) * scrollPortion);
             return (
-                <div>
-                    <Filters />
-                    <ul ref={this.containerRef}>{items.map(this.renderCharacterItem)}</ul>
-                    <button onClick={reload}>Reload</button>
-                </div>
+                <WindowScroller>
+                    {({height, width, isScrolling, scrollTop}) => (
+                        <div>
+                            <Filters />
+                            <button onClick={reload}>Reload</button>
+                            <div>
+                                <List
+                                    autoHeight={true}
+                                    isScrolling={isScrolling}
+                                    height={height}
+                                    rowCount={charactersList.length}
+                                    rowHeight={100}
+                                    rowRenderer={this.renderCharacterItem}
+                                    scrollTop={scrollTop}
+                                    width={width}
+                                />
+                            </div>
+                        </div>
+                    )}
+                </WindowScroller>
             );
         }
-        private onScroll = () => {
-            const containerDom = this.containerRef.current;
-            if (containerDom !== null) {
-                const {charactersList, loading} = this.props;
-                const needScroll = window.innerHeight + window.scrollY >= containerDom.offsetHeight - 500;
-                if (needScroll && charactersList.length > 0 && !loading) {
-                    this.setState({currentScrollPage: this.state.currentScrollPage + 1});
-                }
-            }
+
+        private renderCharacterItem: ListRowRenderer = ({index, isScrolling, key, style}) => {
+            const {name, bloodStatus} = this.props.charactersList[index];
+            return (
+                <div key={key} style={style}>
+                    <span>{name} </span>
+                    {bloodStatus}
+                </div>
+            );
         };
-        private renderCharacterItem = ({_id, ...character}: ICharacter) => (
-            <li key={_id} style={{marginTop: 50, marginBottom: 50}}>
-                {character.name}
-            </li>
-        );
     }
 );
